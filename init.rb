dbe39ab76a89c7df74b3918c739f6328bdc66733
Robin Spainhour <robin@robinspainhour.com>
2009-07-06 11:26:57 +0200

    Moved code into a module that is included during plugin initialization

--- init.rb
+++ init.rb
@@ -1,1 +1,6 @@
-require File.join(File.dirname(__FILE__), 'lib', 'action_mailer_cache_delivery')
+#require File.join(File.dirname(__FILE__), 'lib', 'action_mailer_cache_delivery')
+require 'action_mailer_cache_delivery'
+
+ActionMailer::Base.class_eval do
+  include ActionMailerCacheDelivery
+end
--- lib/action_mailer_cache_delivery.rb
+++ lib/action_mailer_cache_delivery.rb
@@ -1,19 +1,23 @@
-ActionMailer::Base.class_eval do
-
+module ActionMailerCacheDelivery 
   DELIVERIES_CACHE_PATH =
     File.join(RAILS_ROOT,'tmp','cache','action_mailer_cache_deliveries.cache')
 
+  def self.included(base) 
+    base.extend(ClassMethods)
+  end
+  
   def perform_delivery_cache(mail)
     deliveries << mail
     File.open(DELIVERIES_CACHE_PATH,'w') do |f|
       Marshal.dump(deliveries, f)
     end
   end
-
-  def self.cached_deliveries
-    File.open(DELIVERIES_CACHE_PATH,'r') do |f|
-      Marshal.load(f)
-    end
+  
+  module ClassMethods 
+    def cached_deliveries
+      File.open(DELIVERIES_CACHE_PATH,'r') do |f|
+        Marshal.load(f)
+      end
+    end    
   end
-
 end
