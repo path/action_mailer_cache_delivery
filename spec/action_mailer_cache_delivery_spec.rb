98133074f264a6d38d051c75dfb7b176c495bfd5
Robin Spainhour <robin@robinspainhour.com>
2009-07-06 11:48:41 +0200

    Verify that methods are mixed in

--- spec/action_mailer_cache_delivery_spec.rb
+++ spec/action_mailer_cache_delivery_spec.rb
@@ -1,5 +1,20 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
+class MixinReceiver 
+  include ActionMailerCacheDelivery
+end
+
 describe "ActionMailerCacheDelivery" do 
-  it "should have specs"
+  
+  before do 
+    @obj = MixinReceiver.new
+  end
+  
+  it "should mixin perform_delivery_cache" do 
+    @obj.should respond_to(:perform_delivery_cache)
+  end
+  
+  it "should create class method cached_deliveries" do 
+    MixinReceiver.should respond_to(:cached_deliveries)
+  end
 end
