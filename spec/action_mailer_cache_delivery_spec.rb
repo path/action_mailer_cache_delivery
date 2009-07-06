4f5ba05bff150c4402c1a608d917a4d3f083802c
Robin Spainhour <robin@robinspainhour.com>
2009-07-06 12:43:05 +0200

    Added basic specs verifying the mixin

--- spec/action_mailer_cache_delivery_spec.rb
+++ spec/action_mailer_cache_delivery_spec.rb
@@ -1,20 +1,35 @@
 require File.dirname(__FILE__) + '/spec_helper'
 
-class MixinReceiver 
-  include ActionMailerCacheDelivery
+require 'action_mailer'
+
+ActionMailer::Base.send(:include, ActionMailerCacheDelivery)
+ActionMailer::Base.template_root = File.dirname(__FILE__) + '/mail_templates'
+
+class OurMailer < ActionMailer::Base 
+  def testmail
+  end
 end
 
 describe "ActionMailerCacheDelivery" do 
   
   before do 
-    @obj = MixinReceiver.new
+    ActionMailer::Base.delivery_method = :test # Just in case....
+    @mail = OurMailer.create_testmail
   end
   
-  it "should mixin perform_delivery_cache" do 
-    @obj.should respond_to(:perform_delivery_cache)
+  it "should create class method cached_deliveries when included" do 
+    ActionMailer::Base.should respond_to(:cached_deliveries)
   end
   
-  it "should create class method cached_deliveries" do 
-    MixinReceiver.should respond_to(:cached_deliveries)
+  it "should deliver mail to our cache when delivery_method is set to :cache" do 
+    ActionMailer::Base.delivery_method = :cache
+    OurMailer.any_instance.expects(:perform_delivery_cache).with(@mail)
+    OurMailer.deliver(@mail)
+  end
+  
+  it "should not deliver mail to our cache when delivery_method is not set to :cache" do 
+    ActionMailer::Base.delivery_method = :test
+    OurMailer.any_instance.expects(:perform_delivery_cache).never
+    OurMailer.deliver(@mail)
   end
 end
